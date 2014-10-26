a00370d5cd3adbccfb978bf6c979083714ca4fe8
BerserkEva <bakaperv@gmail.com>
2014-10-26 14:48:38 -0700

    Screen auto resizes

---
+++ Productivity/Assets/scripts/ProfileGUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+[System.Serializable]
+public class ProfileGUI : MonoBehaviour {
+
+	void onGUI()
+	{
+		if(GUI.Button (new Rect (500, 500, 100, 100), "BS!!!!"))
+			Debug.Log ("Clicked button to BS someone");
+	}
+
+}
